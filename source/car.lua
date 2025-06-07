0a5efdedb44e4bb928ce27ec879b545055f61bb7
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2025-06-07 11:59:34 -0400

    Car draws over background

--- source/car.lua
+++ source/car.lua
@@ -9,7 +9,8 @@ local gfx = pd.graphics
 class('Car').extends(gfx.sprite)
 
 function Car:init(x, y)
-    local carImage = gfx.image.new("images/integra")
+    local carImage = gfx.image.new("images/cars/integra")
+    self:setImageDrawMode(playdate.graphics.kDrawModeCopy)
     self:setImage(carImage)
     self:moveTo(x, y)
     self:add()
--- source/main.lua
+++ source/main.lua
@@ -11,15 +11,25 @@ import "car"
 local pd = playdate
 local gfx = pd.graphics
 
-local playerX = 40
+local playerX = 200
 local playerY = 120
 
 Game_Intro()
 
 Car(playerX, playerY)
 
+function draw_patterned_background(x, y, width, height)
+    gfx.setPattern({0xFF, 0xC9, 0x80, 0x80, 0xC1, 0xE3, 0xF7, 0xFF})
+    gfx.fillRect(x, y, width, height)
+end
+
+playdate.graphics.sprite.setBackgroundDrawingCallback(draw_patterned_background)
+
 function pd.update()
     gfx.sprite.update()
+
+    playdate.drawFPS(0,0)
+    
     pd.timer.updateTimers()
     -- 30/s (30FPS)
     --carImage:draw(playerX, playerY)
